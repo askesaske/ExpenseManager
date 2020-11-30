b96c695a8d12a5b10c59a7ed71e979a15bff7358
Orazalinov Askhat <58249226+askesaske@users.noreply.github.com>
2020-11-30 19:55:14 +0600

    initial commit, also last
    finished project, crud, filter, report generator added

---
+++ ExpenseManager/Migrations/20201130130554_TestTry.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExpenseManager.Migrations
+{
+    public partial class TestTry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ExpenseManager/Models/ExpenseDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.Models
+{
+    public class ExpenseDBContext : DbContext
+    {
+        /*public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EFGetStarted.ConsoleApp.NewDb;Trusted_Connection=True;");
+            }
+        }*/
+
+        public ExpenseDBContext(DbContextOptions<ExpenseDBContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
